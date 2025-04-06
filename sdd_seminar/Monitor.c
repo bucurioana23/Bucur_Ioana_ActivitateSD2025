5ca06b038cfa6332d65dd2e8b77c2973253a0e09
bucurioana23@stud.ase.ro <bucurioana23@stud.ase.ro>
2025-04-06 17:00:30 +0300

    Functie de stergere cu afisare a monitoarelor din lista

--- sdd_seminar/Monitor.c
+++ sdd_seminar/Monitor.c
@@ -130,7 +130,7 @@ Monitor stergeMonitorDinListaDupaCod(Nod** lista, int cod)
 				aux = aux->next;
 			} while (aux != (*lista) && aux->next->info.cod != cod);
 
-			if (aux->next != (*lista) && aux->info.cod == cod)
+			if (aux->next != (*lista) && aux->next->info.cod == cod)
 			{
 				Nod* p = aux->next;
 				aux->next = p->next;
@@ -140,6 +140,82 @@ Monitor stergeMonitorDinListaDupaCod(Nod** lista, int cod)
 		}	
 	}
 	return initializare(0, "", 0, 0);
+}
+
+void stergeMonitoareCuDiagonalaMaiMareCaCeaData(Nod** lista, float diagonala)
+{
+	if ((*lista) && (*lista)->next == (*lista) && (*lista)->info.diagonala > diagonala)
+	{
+		printf("\nSe sterge acest monitor pt ca are diagonala %.2f > diagonala cautata %.2f\n", (*lista)->info.diagonala, diagonala);
+		afisareMonitor((*lista)->info);
+		if ((*lista)->info.producator != NULL)
+		{
+			free((*lista)->info.producator);
+		}
+		(*lista) = NULL;
+	}
+	else
+	{
+		while ((*lista) && (*lista)->next != (*lista) && (*lista)->info.diagonala > diagonala)
+		{
+			printf("\nSe sterge acest monitor pt ca are diagonala %.2f > diagonala cautata %.2f\n", (*lista)->info.diagonala, diagonala);
+			afisareMonitor((*lista)->info);
+			Nod* p = (*lista)->next;
+			Nod* aux = (*lista);
+			while (p->next != (*lista))
+			{
+				p = p->next;
+			}
+			(*lista) = (*lista)->next;
+			p->next = (*lista);
+			if (aux->info.producator != NULL)
+			{
+				free(aux->info.producator);
+			}
+			free(aux);
+		}
+
+		if (*lista)
+		{
+			Nod* p = (*lista);
+			while (p)
+			{
+				while (p->next != (*lista) && p->next->info.diagonala < diagonala)
+				{
+					p = p->next;
+				}
+				if (p->next != (*lista))
+				{
+					Nod* aux = p->next;
+					printf("\nSe sterge acest monitor pt ca are diagonala %.2f > diagonala cautata %.2f\n", aux->info.diagonala, diagonala);
+					afisareMonitor(aux->info);
+					p->next = aux->next;
+					if (aux->info.producator != NULL)
+					{
+						free(aux->info.producator);
+					}
+					free(aux);
+
+				}
+				else if (p->info.diagonala >diagonala)
+				{
+					printf("\nSe sterge acest monitor pt ca are diagonala %.2f > diagonala cautata %.2f\n", p->info.diagonala, diagonala);
+					afisareMonitor(p->info);
+					if (p->info.producator != NULL)
+					{
+						free(p->info.producator);
+					}
+					free(p);
+					p = NULL;
+					(*lista) = NULL;
+				}
+				else
+				{
+					p = NULL;
+				}
+			}
+		}
+	}
 }
 
 int main()
@@ -159,10 +235,12 @@ int main()
 
 	afiseazaLista(lista);
 	
-	Monitor m = stergeMonitorDinListaDupaCod(&lista, 0);
+	stergeMonitoareCuDiagonalaMaiMareCaCeaData(&lista, 30);
 
-	printf("\nMonitorul cu codul cautat : \n");
-	afisareMonitor(m);
+	//Monitor m = stergeMonitorDinListaDupaCod(&lista, 3);
+
+	/*printf("\nMonitorul cu codul cautat : \n");
+	afisareMonitor(m);*/
 
 	printf("\nLista dupa stergere:\n");
 	afiseazaLista(lista);
